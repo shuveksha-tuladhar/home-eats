f5adbb8934b008fd051232dad3fe0c2fcc6492bd
Shuveksha <tuladhar.shuveksha@gmail.com>
2023-08-03 23:07:03 -0700

    Modified index.js and _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,28 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 
+const API_URL = process.env.STRAPI_URL || "http://127.0.0.1:1337";
+
+export const client = new ApolloClient({
+  uri: `${API_URL}/graphql`,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    mutate: {
+      errorPolicy: "all",
+    },
+    query: {
+      errorPolicy: "all",
+    },
+  },
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }

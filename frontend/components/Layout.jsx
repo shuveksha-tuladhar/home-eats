87601cc3970f831ebb5cb985422c5333099010c8
Shuveksha <tuladhar.shuveksha@gmail.com>
2023-08-06 11:09:59 -0700

    Updated header navbar to display username and logout button

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,43 +1,76 @@
+import { useRouter } from "next/router";
+import { useAppContext } from "@/context/AppContext";
+import Cookie from "js-cookie";
+
 import Head from "next/head";
 import Link from "next/link";
 
 function Navigation() {
-  return (
-    <nav className="container mx-auto flex justify-between p-6 px-4">
-      <div className="flex justify-between items-center w-full">
-        <div className="xl:w-1/3">
-          <Link
-            className="block text-lg max-w-max ext-coolGray-500 hover:text-coolGray-900 font-medium"
-            href="/"
-          >
-            Food Order App
-          </Link>
-        </div>
+    const { user, setUser } = useAppContext();
+    const router = useRouter();
 
-        <div className="xl:block xl:w-1/3">
-          <div className="flex items-center justify-end">
-            <Link
-              className="text-coolGray-500 hover:text-coolGray-900 font-medium"
-              href="/"
-            >
-              Home
-            </Link>
-            <Link
-              className="inline-block py-2 px-4 mr-2 leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium rounded-md"
-              href="/login"
-            >
-              Log In
-            </Link>
-            <Link
-              className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
-              href="/register"
-            >
-              Sign Up
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+  function handleLogout() {
+    setUser(null);
+    Cookie.remove("token");
+    router.push("/");
+  }
+
+  return (
+    <header className="bg-green-800">
+        <nav className="flex justify-between p-6 px-4">
+            <div className="flex justify-between items-center w-full mx-16">
+                <div className="xl:w-1/3">
+                <Link
+                    className="block text-2xl max-w-max text-white font-medium"
+                    href="/"
+                >
+                    Food Order App
+                </Link>
+                </div>
+
+                <div className="xl:block xl:w-1/3">
+                    <div className="flex items-center justify-end">
+                        <Link
+                        className="text-gray-50  hover:text-yellow-200 font-bold"
+                        href="/"
+                        >
+                        Home
+                        </Link>
+                        <div className="hxl:block">
+                            {user ? (
+                                <div className="flex items-center justify-end">
+                                    <span className="inline-block py-2 px-4 mr-2 leading-5 text-gray-50  hover:text-gray-100 bg-transparent font-medium rounded-md">
+                                    {user.username}
+                                    </span>
+                                    <button
+                                    className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
+                                    onClick={handleLogout}
+                                    >
+                                    Log Out
+                                    </button>
+                                </div>
+                            ) : (
+                                <div className="flex items-center justify-end">
+                                    <Link
+                                        className="inline-block py-2 px-4 mr-2 leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium rounded-md"
+                                        href="/login"
+                                        >
+                                        Log In
+                                    </Link>
+                                    <Link
+                                        className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
+                                        href="/register"
+                                        >
+                                        Sign Up
+                                    </Link>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
   );
 }
 

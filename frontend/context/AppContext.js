ba4dbf788e6245483f65f61906bc85e6cb5cb8c0
Shuveksha <tuladhar.shuveksha@gmail.com>
2023-08-07 09:54:59 -0700

    Added cart for checkout, items to save and remove from cart and save to state

--- frontend/context/AppContext.js
+++ frontend/context/AppContext.js
@@ -6,7 +6,14 @@ import { client } from "@/pages/_app.js";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+    const cartCookie =
+        Cookie.get("cart") !== "undefined" ? Cookie.get("cart") : null;
+
+    const [user, setUser] = useState(null);
+    const [showCart, setShowCart] = useState(true);
+    const [cart, setCart] = useState(
+    cartCookie ? JSON.parse(cartCookie) : { items: [], total: 0 }
+    );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,11 +23,63 @@ export const AppProvider = ({ children }) => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    Cookie.set("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addItem = (item) => {
+    let newItem = cart.items.find((i) => i.id === item.id);
+    if (!newItem) {
+      const newItem = {
+        quantity: 1,
+        ...item,
+      };
+      setCart((prevCart) => ({
+        items: [...prevCart.items, newItem],
+        total: prevCart.total + item.attributes.priceInCents,
+      }));
+    } else {
+      setCart((prevCart) => ({
+        items: prevCart.items.map((i) =>
+          i.id === newItem.id ? { ...i, quantity: i.quantity + 1 } : i
+        ),
+        total: prevCart.total + item.attributes.priceInCents,
+      }));
+    }
+  };
+
+  const removeItem = (item) => {
+    let newItem = cart.items.find((i) => i.id === item.id);
+    if (newItem.quantity > 1) {
+      setCart((prevCart) => ({
+        items: prevCart.items.map((i) =>
+          i.id === newItem.id ? { ...i, quantity: i.quantity - 1 } : i
+        ),
+        total: prevCart.total - item.attributes.priceInCents,
+      }));
+    } else {
+      setCart((prevCart) => ({
+        items: prevCart.items.filter((i) => i.id !== item.id),
+        total: prevCart.total - item.attributes.priceInCents,
+      }));
+    }
+  };
+
+  const resetCart = () => {
+    setCart({ items: [], total: 0 });
+  };
+
   return (
     <AppContext.Provider
       value={{
         user,
         setUser,
+        cart,
+        addItem,
+        removeItem,
+        resetCart,
+        showCart,
+        setShowCart,
       }}
     >
       {children}

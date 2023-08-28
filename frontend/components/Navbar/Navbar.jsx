c0e33f18b8b1a44a658bb1ecdd22e85f59a14d2c
Shuveksha <tuladhar.shuveksha@gmail.com>
2023-08-28 14:06:31 -0700

    Added logout button

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -1,44 +1,51 @@
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
-
-import styles from './Navbar.module.css'
-import { useEffect, useState } from "react"
+import { useAppContext } from "@/context/AppContext";
+import styles from "./Navbar.module.css";
+import { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
+import Leftbar from "../Leftbar/Leftbar";
 
 export default function Navbar() {
   const [scrollTop, setScrollTop] = useState(true);
-  const [user, setUser] = useState("");
+  const { user, setUser } = useAppContext();
   useEffect(() => {
     const handleScroll = () => {
       const scrollYPosition = window.scrollY;
       if (scrollYPosition === 0) {
-      setScrollTop(false);
-    } else {
-      setScrollTop(true);
-    }
-  };
+        setScrollTop(false);
+      } else {
+        setScrollTop(true);
+      }
+    };
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-    return (
-        <>
-      <nav
-        className={`${styles.navbar} ${
-          scrollTop ? styles.scrolled : ""
-        }`}
-      >
+  const handleSignOut = () => {
+    setUser(''); // Clear the current user
+  };
+
+  return (
+    <>
+      <nav className={`${styles.navbar} ${scrollTop ? styles.scrolled : ""}`}>
         <div className={styles.navbar_branding}>
-          <div >
-            {/* <Burger /> */}
-          </div>
-          <Image
-            src={scrollTop ? "/homeeats-logo-white-on-transparent.png" : "/homeeats-logo-color-on-transparent.png"}
-            alt="brand_logo"
-            width="150"
-            height="50"
-          />
+          <Leftbar scrollTop={scrollTop} />
+          <Link href="/">
+            <Image
+              src={
+                scrollTop
+                  ? "/homeeats-logo-white-on-transparent.png"
+                  : "/homeeats-logo-color-on-transparent.png"
+              }
+              alt="brand_logo"
+              width="150"
+              height="50"
+              className="ml-3"
+            />
+          </Link>
         </div>
         <div className={styles.navbar_menus}>
           <ul className={styles.menus_list}>
@@ -47,58 +54,97 @@ export default function Navbar() {
             {user === "" ? (
               <>
                 <li className={styles.navbar_menu}>
-                  <Link href="/user/signUp">
+                  <Link
+                    href="/register"
+                    className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-3 text-base font-semibold rounded-full inline-flex items-center"
+                  >
                     <button
                       className={`${styles.navbar_button_login} ${
                         scrollTop ? styles.button_black : ""
                       }`}
                     >
-                      <div className={styles.navbar_button_icon}>
-                        {/* <CreateIcon /> */}
-                        <a>Sign Up</a>
-                      </div>
+                      <Image
+                        width="16"
+                        height="16"
+                        src="/edit-pen.svg"
+                        className="mr-3"
+                      />
+                      <div className={styles.navbar_button_icon}>Sign Up</div>
                     </button>
                   </Link>
                 </li>
                 <li className={styles.navbar_menu}>
-                  <Link href="/user/signIn">
+                  <Link
+                    href="/login"
+                    className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-3 text-base font-semibold rounded-full inline-flex items-center"
+                  >
                     <button
                       className={`${styles.navbar_button_login} ${
                         scrollTop ? styles.button_black : ""
                       }`}
                     >
+                      <Image
+                        width="16"
+                        height="16"
+                        src="/user.svg"
+                        className="mr-3"
+                      />
+                      <div className={styles.navbar_button_icon}>Login</div>
+                    </button>
+                  </Link>
+                </li>
+                <li className={styles.navbar_menu}>
+                  <Link
+                    href="/login"
+                    className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-3 text-base font-semibold rounded-full inline-flex items-center"
+                  >
+                    <button
+                      className={`${styles.navbar_button_login} ${
+                        scrollTop ? styles.button_black : ""
+                      }`}
+                    >
+                      <Image
+                        width="24"
+                        height="24"
+                        src="/demo-login.svg"
+                        className="mr-3"
+                      />
                       <div className={styles.navbar_button_icon}>
-                        {/* <PersonIcon /> */}
-                        <a>Log In</a>
+                        Demo Login
                       </div>
                     </button>
                   </Link>
                 </li>
+                <li>
+                  <Cart />
+                </li>
               </>
             ) : (
-              <>
-                
-
-               
-                {accountType !== "Restaurant" ? (
-                  <li className={styles.navbar_menu}>
-                    <Link href="/user/myCart">
-                      <div className={styles.cart_container}>
-                        <span title="Cart">
-                          <AiOutlineShoppingCart
-                            className={styles.navbar_icon}
-                          />
-                        </span>
-                        <span className={styles.cart_quantity}>
-                          {totalCartQuantity}
-                        </span>
-                      </div>
-                    </Link>
-                  </li>
-                ) : (
-                  <></>
-                )}
-
+              <><li>
+              <Cart />
+            </li>
+                <li className={styles.navbar_menu}>
+                  <Link
+                    href=""
+                    className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-3 text-base font-semibold rounded-full inline-flex items-center"
+                  >
+                    <button
+                    onClick={handleSignOut}
+                      className={`${styles.navbar_button_login} ${
+                        scrollTop ? styles.button_black : ""
+                      }`}
+                    >
+                       <Image
+                        width="24"
+                        height="24"
+                        src="/user-circle.svg"
+                        className="mr-3"
+                      />
+                      <div className={styles.navbar_button_icon}>Logout</div>
+                    </button>
+                                 
+                  </Link>      
+                </li>
                 
               </>
             )}
@@ -109,11 +155,11 @@ export default function Navbar() {
         .${styles.scrolled} {
           transition: background-color 0.3s ease-in-out;
         }
-        
+
         .${styles.button_black} .navbar_button_icon a {
           color: black;
         }
       `}</style>
     </>
-    )
+  );
 }
